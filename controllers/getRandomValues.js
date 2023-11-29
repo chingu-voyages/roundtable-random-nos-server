fb8fadf1e3106aa27d3125756a3e91ec08c4fab1
jdmedlock <jdmedlock@gmail.com>
2023-11-28 18:34:21 -0600

    Feat: Add random value controller & route

--- controllers/getRandomValues.js
+++ controllers/getRandomValues.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler')
 const crypto = require('crypto')
 
+//
 const getRandomValues = (async (req, res) => {
   const length = req.query.length
   const randomValues = crypto.getRandomValues(new Uint8Array(length))
--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { getRandomValues } = require('../controllers/getRandomValues')
 
 
-const router = express.Router();
+const router = express.Router()
 
 router.route('/getRandomValues')
     .get(getRandomValues)
